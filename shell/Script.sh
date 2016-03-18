f47d4cc4b051dca85db7f584e51b47c73ea4392c
jiangwei <diablos327@aliyun.com>
2016-03-18 18:20:30 +0800

    modified Script.sh.

--- shell/Script.sh
+++ shell/Script.sh
@@ -10,13 +10,13 @@ setAllUUIDHashXattr()
     for filename in $(ls)
     do
         if [ -f ${filename} ];then
-            setfattr -n user.uuid -v `uuid -v 4` ${filename}
+            setfattr -n user.uuid -v `uuid -v 4` "${filename}"
 
             originalStr=`sha256sum ${filename}`
             processedStr=${originalStr:0:64}
-            setfattr -n user.hash -v ${processedStr} ${filename}
+            setfattr -n user.hash -v "${processedStr}" "${filename}"
         elif [ -d ${filename} ];then
-            setfattr -n user.uuid -v `uuid -v 4` ${filename}
+            setfattr -n user.uuid -v `uuid -v 4` "${filename}"
 
             cd ${filename}
             setAllUUIDHashXattr
@@ -32,7 +32,7 @@ setAllUUIDHashXattr()
 # Parameters: $1 fileName
 getUUIDXattr()
 {
-    getfattr -n user.uuid ${1} --only-values
+    getfattr -n user.uuid "${1}" --only-values
 }
 
 # Set UUID value
@@ -40,7 +40,7 @@ getUUIDXattr()
 # Parameters: $1 fileName; $2 UUIDValue
 setUUIDXattr()
 {
-    setfattr -n user.uuid -v ${2} ${1}    
+    setfattr -n user.uuid -v "${2}" "${1}"    
 }
 
 # Clean UUID value
@@ -48,7 +48,7 @@ setUUIDXattr()
 # Parameters: $1 fileName
 cleanUUIDXattr()
 {
-    setfattr -x user.uuid ${1}     
+    setfattr -x user.uuid "${1}"     
 }
 
 # Get Hash value
@@ -56,7 +56,7 @@ cleanUUIDXattr()
 # Parameters: $1 fileName
 getHashXattr()
 {
-    getfattr -n user.hash ${1} --only-values
+    getfattr -n user.hash "${1}" --only-values
 }
 
 # Set Hash value
@@ -64,7 +64,7 @@ getHashXattr()
 # Parameters: $1 fileName; $2 HashValue
 setHashXattr()
 {
-    setfattr -n user.hash -v ${2} ${1}     
+    setfattr -n user.hash -v "${2}" "${1}"     
 }
 
 # Clean Hash value
@@ -72,9 +72,10 @@ setHashXattr()
 # Parameters: $1 fileName
 cleanHashXattr()
 { 
-    setfattr -x user.hash ${1}
+    setfattr -x user.hash "${1}"
 }
 
+##############################################################
 
 # Add xattr with ownerlist for this file
 # Deal with single file
@@ -91,7 +92,7 @@ addOwnerListXattr()
     fi
 }
 
-# Reset xattr with ownerlist for this file
+# Clean xattr with ownerlist for this file
 # Deal with single file
 # Parameters: $1 fileName
 cleanOwnerListXattr()
@@ -99,6 +100,58 @@ cleanOwnerListXattr()
     setfattr -x user.ownerlist "$1"
 }
 
+# Get xattr with ownerlist for this file
+# Deal with single file
+# Parameters: $1 fileName
+getOwnerListXattr()
+{
+    getfattr -n user.ownerlist "$1"
+}
+
+# Delete xattr with ownerlist for this file
+# Deal with single file
+# Parameters: $1 fileName; $2 ownerlistValue
+deleteOwnerListXattr()
+{
+    originalStr=`getfattr -n user.ownerlist "$1" --only-values`
+    if [ $? != 0 ]
+    then
+        # Empty
+        return 101
+    else
+        oldIFS="$IFS"
+        IFS="; "
+        array=(${originalStr})
+        IFS=${oldIFS}
+        tmpStr=""
+        counter=0
+
+        for name in ${array[@]}
+        do
+            if [ ${name} != ${2} ]
+            then
+                if [ ${counter} -eq 0 ]
+                then
+                    tmpStr=${name}
+                    let counter+=1
+                else
+                    tmpStr="${tmpStr}; ${name}"
+                    let counter+=1
+                fi
+            else
+                continue
+            fi
+        done
+
+        if [ ${counter} -eq ${#array[@]} ]
+        then
+            return 102
+        else
+            setfattr -x user.ownerlist "${1}"
+            setfattr -n user.ownerlist -v "${tmpStr}" "${1}"
+        fi
+    fi
+}
 
 
 
