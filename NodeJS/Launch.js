266433347911512b07569e8800a0dcd77d47301f
John <wei.jiang@winsuntech.cn>
2016-03-21 18:35:32 +0800

    Create Launch.js

---
+++ NodeJS/Launch.js
@@ -0,0 +1,8 @@
+
+var traverse = require('./Traverse.js');
+var handleFile = require('./HandleFile.js');
+
+//traverse.Traverse('./', handleFile.SetUUIDXattr);
+//traverse.Traverse('./', handleFile.SetHashXattr);
+
+handleFile.AddReadlistXattr('hehehe', 'hello');
