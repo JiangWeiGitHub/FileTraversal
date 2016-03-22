1efa5e1903dbbbce14ebbcccbdc7716ab5864163
John <wei.jiang@winsuntech.cn>
2016-03-22 16:18:17 +0800

    Update HandleFile.js

--- NodeJS/HandleFile.js
+++ NodeJS/HandleFile.js
@@ -104,6 +104,101 @@ exports.SetUUIDXattr = SetUUIDXattr;
 /******************************************************/
 
 /*
+  Function: Add xattr with user.type for a normal file or folder
+  Parameters: path -> specified from caller which to deal with
+*/  
+
+function SetTypeXattr(path)
+{
+    var filename = path;
+
+    fs.stat(path, function(err, stats)
+    {
+        if (err)
+        {
+            console.log('Get State Error!');
+        }
+        else
+        {  
+            if (stats.isDirectory())
+            {
+                xattr.set(path, 'user.type', 'folder', function(err)
+                {
+                    if (err)
+                    {
+                        console.log('Set Xattr Failed!');
+                    
+                        //return console.error(err);
+                    }
+                });
+            }
+            else if (stats.isFile())
+            {
+                xattr.set(path, 'user.type', 'file', function(err)
+                {
+                    if (err)
+                    {
+                        console.log('Set Xattr Failed!');
+                    
+                        //return console.error(err);
+                    }
+                });
+            }
+            else
+            {
+                console.log('Unsupported File Format!');
+            }
+        }
+    })  
+}
+
+exports.SetTypeXattr = SetTypeXattr;
+
+/******************************************************/
+
+/*
+  Function: Add xattr with user.owner for a normal file or folder
+  Parameters: path -> specified from caller which to deal with
+              value -> owner's new value
+*/  
+
+function SetOwnerXattr(path, value)
+{
+    var filename = path;
+
+    fs.stat(path, function(err, stats)
+    {
+        if (err)
+        {
+            console.log('Get State Error!');
+        }
+        else
+        {  
+            if (stats.isDirectory() || stats.isFile())
+            {
+                xattr.set(path, 'user.owner', value, function(err)
+                {
+                    if (err)
+                    {
+                        console.log('Set Xattr Failed!');
+                    
+                        //return console.error(err);
+                    }
+                });
+            }
+            else
+            {
+                console.log('Unsupported File Format!');
+            }
+        }
+    })  
+}
+
+exports.SetOwnerXattr = SetOwnerXattr;
+
+/******************************************************/
+
+/*
   Function: Add xattr with user.readlist for a specified file or folder
   Parameters: path -> specified from caller which to deal with
               value -> readlist's new value
@@ -191,5 +286,96 @@ function AddReadlistXattr(path, value)
 }
 
 exports.AddReadlistXattr = AddReadlistXattr;
+
+/******************************************************/
+
+/*
+  Function: Add xattr with user.writelist for a specified file or folder
+  Parameters: path -> specified from caller which to deal with
+              value -> writelist's new value
+*/  
+
+function AddWritelistXattr(path, value)
+{
+    var filename = path;
+
+    fs.stat(path, function(err, stats)
+    {
+        if (err)
+        {
+            console.log('Get State Error!');
+        }
+        else
+        {  
+            if (stats.isDirectory() || stats.isFile())
+            {
+                // get xattr first
+                xattr.get(path, 'user.writelist', function(err, get_value)
+                {
+                    if (err)
+                    {
+                        console.log('Get Xattr Value Failed!');
+
+                        xattr.set(path, 'user.writelist', '[' + value + ']', function(err)
+                        {
+                            if (err)
+                            {
+                                console.log('Set Xattr [user.writelist] Value Failed!');
+                    
+                                //return console.error(err);
+                            }
+                        });
+                    
+                        //return console.error(err);
+                    }
+                    else
+                    {
+                        if (get_value.indexOf("]") <= 0)
+                        {
+                            xattr.set(path, 'user.writelist', '[' + value + ']', function(err)
+                            {
+                                if (err)
+                                {
+                                    console.log('Set Xattr [user.writelist] Value Failed!');
+                    
+                                    //return console.error(err);
+                                }
+                            });
+                        }
+                        else
+                        {
+                            if (get_value.indexOf(value) <= 0)
+                            {
+                                var tmp = get_value.toString();
+
+                                var tmp_value = tmp.substr(0, get_value.length - 1);
+
+                                xattr.set(path, 'user.writelist', tmp_value + ',' + value + ']', function(err)
+                                {
+                                    if (err)
+                                    {
+                                        console.log('Set Xattr [user.writelist] Value Failed!');
+                    
+                                        //return console.error(err);
+                                    }
+                                });
+                            }
+                            else
+                            {
+                                console.log('Xattr [user.writelist] Value Already exists!');
+                            }
+                        }
+                    }
+                });
+            }
+            else
+            {
+                console.log('Unsupported File Format!');
+            }
+        }
+    })  
+}
+
+exports.AddWritelistXattr = AddWritelistXattr;
 
 /******************************************************/
